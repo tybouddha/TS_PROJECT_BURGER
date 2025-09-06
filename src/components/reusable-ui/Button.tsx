b2e10fa01c0c69ebf706372f796ccc9670ec40eb
Tybouddha <bruneecedric@gmail.com>
2025-09-06 07:31:47 +0200

    ♻️ TSE-02: Add SelectInput.tsx

--- src/components/reusable-ui/Button.tsx
+++ src/components/reusable-ui/Button.tsx
@@ -1,5 +1,5 @@
 import styled, { css } from "styled-components";
-import { theme } from "../../theme";
+import { theme } from "../../theme/theme";
 import { ComponentProps } from "react";
 
 type ButtonVersionType = "normal" | "success";

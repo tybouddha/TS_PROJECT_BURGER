a8f41b9cac224c633e6bfe42d64f7a89fb8eb1e3
Tybouddha <bruneecedric@gmail.com>
2025-09-11 14:07:40 +0200

    ♻️ TSE-08-PAGES: Add ErrorPage.tsx

---
+++ src/routers/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import LoginPage from "../components/pages/login/LoginPage";
+import OrderPage from "../components/pages/order/OrderPage";
+import ErrorPage from "@/components/pages/error/ErrorPage";
+
+const routes: RouteObject[] = [
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/order/:username",
+    element: <OrderPage />,
+  },
+  {
+    path: "*", // "catch-all" route
+    element: <ErrorPage />,
+  },
+];
+
+export const router = createBrowserRouter(routes);

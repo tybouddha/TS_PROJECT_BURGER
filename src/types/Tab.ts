6ee3cb73cd15dc4e770529beef2b292861e79054
Tybouddha <bruneecedric@gmail.com>
2025-09-13 22:43:40 +0200

    ♻️ TSE-14-FORM: Add AdminTabs.tsx

--- src/types/Tab.ts
+++ src/types/Tab.ts
@@ -1,1 +1,12 @@
-export type AdminTabValueType = "add" | "edit" | "chevron";
+import { AdminTabEnum } from "@/enums/tab";
+
+// export type AdminTabValueType = "add" | "edit" | "chevron";
+
+export type TabType = {
+  index: AdminTabEnum;
+  label: string;
+  Icon?: JSX.Element;
+  Content?: JSX.Element;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  className?: string;
+};

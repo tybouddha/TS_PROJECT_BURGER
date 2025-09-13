f1e0b8d40a3768ae57bffbf35110ed649da3a408
Tybouddha <bruneecedric@gmail.com>
2025-09-13 07:54:12 +0200

    ♻️ TSE-12-BASKET:  Add BasketProducts.tsx

--- src/components/pages/order/Main/Basket/BasketBody/BasketProducts.tsx
+++ src/components/pages/order/Main/Basket/BasketBody/BasketProducts.tsx
@@ -1,20 +1,15 @@
-//@ts-nocheck
-import React from "react";
 import styled from "styled-components";
-import {
-  BASKET_MESSAGE,
-  IMAGE_COMING_SOON,
-} from "../../../../../../enums/product";
+import { BASKET_MESSAGE, IMAGE_COMING_SOON } from "@/enums/product";
 import BasketCard from "./BasketCard";
-import { useOrderContext } from "../../../../../../context/OrderContext";
-import { findObjectById } from "../../../../../../utils/array";
+import { useOrderContext } from "@/context/OrderContext";
+import { findObjectById } from "@/utils/array";
 import { checkIfProductIsClicked } from "../../MainRightSide/Menu/helper";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
-import { basketAnimation } from "../../../../../../theme/animations";
-import { formatPrice } from "../../../../../../utils/maths";
-import { convertStringToBoolean } from "../../../../../../utils/string";
-import Sticker from "../../../../../reusable-ui/Sticker";
+import { basketAnimation } from "@/theme/animations";
+import { formatPrice } from "@/utils/maths";
+import { convertStringToBoolean } from "@/utils/string";
 import { useParams } from "react-router-dom";
+import { MenuProductType } from "@/types/Product";
 
 export default function BasketProducts() {
   const {
@@ -28,18 +23,34 @@ export default function BasketProducts() {
 
   const { username } = useParams();
 
-  const handleOnDelete = (event, id) => {
+  const handleOnDelete = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    id: string
+  ) => {
     event.stopPropagation();
-    handleDeleteBasketProduct(id, username);
+    username && handleDeleteBasketProduct(id, username);
+  };
+
+  const handleClickCard = (isModeAdmin: boolean, basketProductId: string) => {
+    isModeAdmin && handleProductSelected(basketProductId);
+  };
+
+  const getPrice = (menuProduct: MenuProductType) => {
+    return convertStringToBoolean(menuProduct.isAvailable)
+      ? formatPrice(menuProduct.price)
+      : BASKET_MESSAGE.NOT_AVAILABLE;
   };
 
   return (
+    //@ts-expect-error
     <TransitionGroup
       component={BasketProductsStyled}
       className={"transition-group"}
     >
       {basket.map((basketProduct) => {
+        if (menu === undefined) return;
         const menuProduct = findObjectById(basketProduct.id, menu);
+        if (!menuProduct) return;
         return (
           <CSSTransition
             appear={true}
@@ -58,21 +69,13 @@ export default function BasketProducts() {
                 quantity={basketProduct.quantity}
                 onDelete={(event) => handleOnDelete(event, basketProduct.id)}
                 isClickable={isModeAdmin}
-                onClick={
-                  isModeAdmin
-                    ? () => handleProductSelected(basketProduct.id)
-                    : null
-                }
+                onClick={() => handleClickCard(isModeAdmin, basketProduct.id)}
                 isSelected={checkIfProductIsClicked(
                   basketProduct.id,
                   productSelected.id
                 )}
                 className={"card"}
-                price={
-                  convertStringToBoolean(menuProduct.isAvailable)
-                    ? formatPrice(menuProduct.price)
-                    : BASKET_MESSAGE.NOT_AVAILABLE
-                }
+                price={getPrice(menuProduct)}
                 isPublicised={convertStringToBoolean(menuProduct.isPublicised)}
               />
             </div>

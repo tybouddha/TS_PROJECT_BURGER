0a952eaa7c69c2b3c06e0761a88c5ba0529633f2
Tybouddha <bruneecedric@gmail.com>
2025-09-14 07:26:01 +0200

    ♻️ TSE-14-FORM: Add EditForm.tsx

---
+++ src/components/pages/order/Main/MainRightSide/Admin/AdminPanel/EditForm/EditForm.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { useOrderContext } from "@/context/OrderContext";
+import Form from "../Form/Form";
+import EditInfoMessage from "./EditInfoMessage";
+import SavingMessage from "./SavingMessage";
+import { useSuccessMessage } from "@/hooks/useSuccessMessage";
+import { useParams } from "react-router-dom";
+
+export default function EditForm() {
+  // state
+  const { productSelected, setProductSelected, handleEdit, titleEditRef } =
+    useOrderContext();
+
+  const { username } = useParams();
+
+  const [valueOnFocus, setValueOnFocus] = useState<string>();
+  const { isSubmitted: isSaved, displaySuccessMessage } = useSuccessMessage();
+
+  // comportements (gestionnaires d'événement ou "event handlers")
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const { name, value } = event.target;
+
+    const productBeingUpdated = {
+      ...productSelected,
+      [name]: value,
+    };
+
+    setProductSelected(productBeingUpdated); // cette ligne update le formulaire
+
+    username && handleEdit(productBeingUpdated, username); // cette ligne update le menu
+  };
+
+  const handleOnFocus = (
+    event: React.FocusEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const valueOnFocus = event.target.value;
+    setValueOnFocus(valueOnFocus);
+  };
+
+  const handleOnBlur = (
+    event: React.FocusEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
+    const valueOnBlur = event.target.value;
+    if (valueOnFocus !== valueOnBlur) {
+      console.log("ça a changé !!");
+      displaySuccessMessage();
+    }
+  };
+
+  // affichage
+  return (
+    <Form
+      product={productSelected}
+      onChange={handleChange}
+      onFocus={handleOnFocus}
+      onBlur={handleOnBlur}
+      ref={titleEditRef}
+    >
+      {isSaved ? <SavingMessage /> : <EditInfoMessage />}
+    </Form>
+  );
+}
